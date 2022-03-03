ead9b640f757c234b59fdea1c69725587e63dcd1
draimius <jrsworkshop17@gmail.com>
2022-03-03 14:12:52 -0600

    nextjs real estate site

--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,33 @@
 //extra stuff needed if using the nextjs with chakra(import and tag in app render)
 import { ChakraProvider } from '@chakra-ui/react';
 import Router from 'next/router';
+import Head from 'next/head';
 import NProgress from 'nprogress';
 
 import Layout from '../components/Layout';
 
-// import { Html, Head, Main, NextScript } from 'next/document';
-
-// export function Document() {
-//   return (
-//     <Html>
-//       <Head>
-//         <link
-//           rel="stylesheet"
-//           href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-//           integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
-//           crossOrigin="anonymous"
-//           referrerPolicy="no-referrer"
-//         />
-//       </Head>
-//       <body>
-//         <Main />
-//         <NextScript />
-//       </body>
-//     </Html>
-//   );
-// }
-
 function MyApp({ Component, pageProps }) {
-  //component for 'loading display' initialize to false
   NProgress.configure({ showSpinner: false });
 
-  //nextjs specific even we can track (on a route/path change we dispay loader
   Router.events.on('routeChangeStart', () => {
     NProgress.start();
   });
-  //then when page done loading/rendering data we remove the loader from display (both with specified event listeners)
+
   Router.events.on('routeChangeComplete', () => {
     NProgress.done();
   });
 
   return (
     <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
       <ChakraProvider>
         <Layout>
           <Component {...pageProps} />
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const baseUrl = process.env.BASE_URL;
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.RAPID_API_KEY,
+      'x-rapidapi-key': '0e021b9f04msh6bb87ad79e3dd5cp105330jsnad699f1b1a6a',
     },
   });
   return data;
